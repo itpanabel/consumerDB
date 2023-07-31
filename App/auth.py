b37fb27fe1c85d17db9d383e56c1062a3a4a9e1d
Alan Montoya <alan.montoya@panabel.com>
2023-07-31 10:27:13 -0500

    Project Initial Setup

---
+++ App/auth.py
@@ -0,0 +1,120 @@
+import functools
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from App.db import get_db
+
+# Create Blueprint auth to
+# manage user login session
+# status.
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+def login_required(view):
+    """View decorator that redirects
+    anonymous users to the login page.
+    """
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+    return wrapped_view
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    """If a user id is stored in the session,
+    load the user object from database into
+    ``g.user``.
+    """
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db().execute(
+                "SELECT * FROM USERS WHERE id = ?", (user_id,)
+            ).fetchone()
+        )
+
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    """Register new user in database.
+    Validate if the username is not
+    taken. Hashes the password for
+    security.
+    """
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "Nombre de usuario es requerido"
+        elif not password:
+            error = "Contraseña es requerida"
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO USERS (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                # The username was already taken, which caused the
+                # commit to fail. Show a validation error.
+                error = f"El usuario {username} ya existe."
+            else:
+                # Sucess, go the login page.
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template("auth/register.html")
+
+
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    """Log in registered user by adding the
+    user id to the session.
+    """
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM USERS WHERE username = ?",
+            (username,)
+        ).fetchone()
+
+        if user is None:
+            error = "Nombre de usuario no puede estar vacio."
+        elif not check_password_hash(user["password"], password):
+            error = "Usuario o contraseña incorrectos."
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+
+    return render_template("auth/login.html")
+
+
+@bp.route("/logout")
+def logout():
+    """Clear the current session,
+    including the stored user id.
+    """
+    session.clear()
+    return redirect(url_for("index"))
---
+++ App/static/scripts/main.js
@@ -0,0 +1,113 @@
+/**
+ * Set Domains for suggest
+ * email completation for
+ * customers
+ */
+$("#email").emailautocomplete({
+suggClassName: "email-autocomplete",
+domains: [
+    "yahoo.com",
+    "hotmail.com",
+    "gmail.com",
+    "me.com",
+    "aol.com",
+    "mac.com",
+    "live.com",
+    "comcast.net",
+    "googlemail.com",
+    "msn.com",
+    "hotmail.co.uk",
+    "yahoo.co.uk",
+    "facebook.com",
+    "verizon.net",
+    "sbcglobal.net",
+    "att.net",
+    "gmx.com",
+    "outlook.com",
+    "icloud.com",
+    "panabel.com"
+]
+});
+
+
+/**
+ * Geolocalization
+ */
+// let LATITUDE = 0.00;
+// let LONGITUDE = 0.00
+// let geodata = navigator.geolocation;
+
+// function successGeoData(position) {
+//   const myLatitude = position.coords.latitude;
+//   const myLongitude = position.coords.longitude;
+//   let geodataMsg = "Geo data obtained";
+//   updateGeoData(myLatitude, myLongitude);
+// }
+
+// function errorGeoData(error) {
+//   let geodataMsg = "Unable to obtain Geo Data";
+// }
+
+// function updateGeoData(lat, lon) {
+//   LATITUDE = lat;
+//   LONGITUDE = lon;
+// }
+// console.log(geodata.getCurrentPosition(successGeoData, errorGeoData));
+
+
+
+/**
+ * Obtain IP Address
+ */
+
+let myIP;
+fetch('https://api.ipify.org?format=json')
+    .then((response) => response.json())
+    .then((data) => myIP = data.ip)
+    .catch((error) => console.error);
+console.log("My public IP is: ", myIP);
+
+/**
+ * unhide and hide Guerlain
+ * options
+ * @returns nothing
+ */
+function unhideGuerlain() {
+let guerlainsection = document.querySelector("#guerlainSpecific");
+let guerlainCheckboxes = document.getElementsByName("guerlain-specific");
+if (guerlainsection.style.display === "block") {
+    guerlainsection.style.display = "none";
+    for (i = 0; i < guerlainCheckboxes.length; i++) {
+    guerlainCheckboxes[i].checked = false;
+    }
+} else {
+    guerlainsection.style.display = "block";
+}
+}
+
+/**
+ * Format date for Birthday
+ * @param mydate datetime object
+ * @returns String MM/DD
+ */
+function bdayFormat(mydate) {
+    let month = String(mydate.getMonth()+1).padStart(2, '0');
+    let day = String(mydate.getDate()).padStart(2, '0');
+    return `${month}/${day}`
+}
+
+
+/**
+ * Format date to ISO 8601
+ * @param mydate type datetime
+ * @returns string YYYY-MM-DD HH:mm:ss
+ */
+function isoDate(mydate) {
+    let year = mydate.getFullYear();
+    let month = String(mydate.getMonth()+1).padStart(2, '0');
+    let day = String(mydate.getDate()).padStart(2, '0');
+    let hours = String(mydate.getHours()).padStart(2, '0');
+    let minutes = String(mydate.getMinutes()).padStart(2, '0');
+    let seconds = String(mydate.getSeconds()).padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
